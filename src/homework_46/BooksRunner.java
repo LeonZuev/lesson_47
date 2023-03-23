7d948ca03c60977bf0e363cedbaa0b2ffa786f5a
Leon Zuev <loonatune@gmail.com>
2023-03-23 19:49:28 +0200

    Finish homework

--- src/homework_46/BooksRunner.java
+++ src/homework_46/BooksRunner.java
@@ -1,4 +1,18 @@
 package homework_46;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BooksRunner {
+  public static void main(String[] args) {
+    List<Book> books = new ArrayList<>();
+    books.add(new Book("Букварь", "Неизвестно",125));
+    books.add(new Book("Букварь", "Народ",25));
+    books.add(new Book("АИ", "Иа",5));
+
+    books.sort(new BookTitleAuthorComparator());
+    for (Book b: books) {
+      System.out.println(b);
+    }
+  }
 }
