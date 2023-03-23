5fc1559d414d72380c96336bfc1b2b8e13658c5f
Leon Zuev <loonatune@gmail.com>
2023-03-23 21:34:19 +0200

    Update CalculatorTests

--- src/Calculator/CalculatorTests.java
+++ src/Calculator/CalculatorTests.java
@@ -4,18 +4,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 public class CalculatorTests {
 
+  // arrange - упорядочить, установить
+  private Calculator calc = new Calculator();
+
   // сложение положительных целых чисел
   @Test
   public void addPositiveIntegers() { // название теста
-    // arrange - упорядочить, установить
-    Calculator calc = new Calculator();
-
     // act - выполнить действие
-    int result = calc.add(2,5);
+    int result = calc.add(2, 5);
 
     // assert - сравнить результат с правильным
     // assertEquals(expected, actual)
-    // предположитьРавенство(ожиданиеб реальность)
+    // предположитьРавенство(ожидание, реальность)
     assertEquals(7, result);
+
+    // все три этапа в одной строке:
+    assertEquals(4, calc.add(2, 2));
   }
-}
+
+  @Test
+  public void addNegativeIntegers() {
+    assertEquals(-5, calc.add(-2, -3));
+    assertEquals(-5, calc.add(-3, -2));
+  }
+
+  @Test
+  public void addNegativeZero() {
+    assertEquals(-5, calc.add(-5, 0));
+    assertEquals(-5, calc.add(0, -5));
+  }
+
+  @Test
+  public void addCommutative() {
+    // можно сравнивать не ожидание и реальность, а две реальности
+    assertEquals(calc.add(3, 57), calc.add(57, 3));
+  }
+
+  @Test
+  public void addPositiveZero() {
+    assertEquals(5, calc.add(5, 0));
+    assertEquals(5, calc.add(0, 5));
+  }
+
+  @Test
+  public void addZeros() {
+    assertEquals(0, calc.add(0, 0));
+  }
+}}
