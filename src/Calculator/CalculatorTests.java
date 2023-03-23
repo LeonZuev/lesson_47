4da86630115241529ccad4d2555ab7578a2ef988
Leon Zuev <loonatune@gmail.com>
2023-03-23 20:59:10 +0200

    Update addPositiveIntegers

--- src/Calculator/CalculatorTests.java
+++ src/Calculator/CalculatorTests.java
@@ -4,8 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 public class CalculatorTests {
 
+  // сложение положительных целых чисел
   @Test
-  public void AddPositiveIntegers() {
+  public void addPositiveIntegers() { // название теста
+    // arrange - упорядочить, установить
+    Calculator calc = new Calculator();
 
+    // act - выполнить действие
+    int result = calc.add(2,5);
+
+    // assert - сравнить результат с правильным
+    // assertEquals(expected, actual)
+    // предположитьРавенство(ожиданиеб реальность)
+    assertEquals(7, result);
   }
 }
