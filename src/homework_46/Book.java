4c4f04e68864a880b055ff1274655f14e0d91403
Leon Zuev <loonatune@gmail.com>
2023-03-23 18:41:58 +0200

    Add Book

---
+++ src/homework_46/Book.java
@@ -0,0 +1,34 @@
+package homework_46;
+
+public class Book {
+  private final String title;
+  private final String author;
+  private final int pages;
+
+  public Book(String title, String author, int pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public int getPages() {
+    return pages;
+  }
+
+  @Override
+  public int compareTo(Book o) {
+    if(!author.equals(o.author)) {
+      return author.compareTo(o.author);
+    }
+    return title.compareTo(o.title);
+  }
+}
+
