e36a3362ada8c0112c4ae430d3c08df72181eb96
Leon Zuev <loonatune@gmail.com>
2023-03-23 19:12:04 +0200

    Add BookTitleAuthorComparator

---
+++ src/homework_46/BookTitleAuthorComparator.java
@@ -0,0 +1,11 @@
+package homework_46;
+
+import java.util.Comparator;
+
+public class BookTitleAuthorComparator implements Comparator<Book> {
+
+  @Override
+  public int compare(Book o1, Book o2) {
+
+  }
+}
