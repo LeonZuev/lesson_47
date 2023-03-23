fd914f5585ba9ab0a371995c316f8974c4cd9b2d
Leon Zuev <loonatune@gmail.com>
2023-03-23 20:46:42 +0200

    Add CalculatorTests

--- src/Calculator/CalculatorTests.java
+++ src/Calculator/CalculatorTests.java
@@ -1,4 +1,11 @@
 package Calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 public class CalculatorTests {
+
+  @Test
+  public void Add_2Plus5_7Returned() {
+
+  }
 }
